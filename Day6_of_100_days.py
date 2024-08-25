fc4ba484f47fc07105683325cdaf1dfee05260e4
Utkarsh Singh <utkarshsingh.0902@gmail.com>
2024-08-25 22:56:36 +0530

    Update Day6_of_100_days.py

---
+++ Day6_of_100_days.py
@@ -0,0 +1,30 @@
+# Day 6 of 100 days
+# PYTHON BOOTCAMP
+
+# PYTHON FUNCTION AND KAREL
+
+# Build in function 
+
+#Function:- A function is a block of code which only runs when it is called. You can pass data, known as parameters, into a function.
+
+num_char=len("Hello")
+print(num_char) #output=5
+
+# if we have to make our own function:- 
+# def keyboard with function name:-
+ 
+def my_function():
+    print("hello")
+    print("bye")
+    
+my_function()
+
+# On day 6 i learn about a while loops with real life implicant i.e
+# Reeborgs World start games, like
+
+# 1. Hurdles Loop Challenge.
+# 1.2 using while loop hurdles challenge
+#2. Jumping over Hurdles with variable heights.
+# 3. And the final project which is about Escaping The maze 
+
+# The following link is in description.

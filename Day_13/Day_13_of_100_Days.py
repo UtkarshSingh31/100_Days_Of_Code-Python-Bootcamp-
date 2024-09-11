e24691561245818d9b6f6c0b58c80a9566e065bb
Utkarsh Singh <utkarshsingh.0902@gmail.com>
2024-09-11 15:52:01 +0530

    Add files via upload

---
+++ Day_13/Day_13_of_100_Days.py
@@ -0,0 +1,36 @@
+#Day 13 of 100 days 
+# Pythoon Bootcamp 
+
+# Debuging programs.
+#1
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
+#2
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+  
+#3
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
